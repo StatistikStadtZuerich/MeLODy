21c62afea99991266ff402e61512f825e3c2c5f0
Jan Cortiel <jan.cortiel@redlink.at>
2024-11-13 09:04:34 +0100

    Removed dist folder

--- dist/utils/stringUtils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toValidString = void 0;
-const toValidString = (input) => input && input.toString().trim() !== "" && input.toString() !== '*' ? input.toString() : undefined;
-exports.toValidString = toValidString;
